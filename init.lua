f9738c6062bb0c90eec68c779438ad627d8e7638
David Britt <djmb102@gmail.com>
2025-05-24 13:39:34 -0400

    Move files to their proper locations

--- init.lua
+++
@@ -1,6 +0,0 @@
--- init.lua
--- Entry point for anki-practice.nvim
-
-local M = require("anki-practice")
-
-return M
---
+++ lua/vimki/init.lua
@@ -0,0 +1,6 @@
+-- init.lua
+-- Entry point for vimki.nvim
+
+local M = require("vimki")
+
+return M
